0bba6d40232fa2a4ef018e2e1470e9af057b8fca
Android Studio <devandroid@ort>
2025-08-08 10:22:36 -0300

    cambio

--- API-Integrador/src/App.js
+++ API-Integrador/src/App.js
@@ -1,34 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
 const express = require('express');
 const cors = require('cors');
 const rutasAutenticacion = require('./rutas/autenticacionRutas');
 const rutasEvento = require('./rutas/eventoRutas');
 
 const app = express();
-app.use(cors());
+
+// Middleware
+app.use(cors()); 
 app.use(express.json());
 
+// Rutas
 app.use('/api/usuario', rutasAutenticacion);
 app.use('/api/evento', rutasEvento);
 
 module.exports = app;
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
